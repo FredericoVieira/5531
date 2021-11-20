b21f3dc302370fe7e8c335a6987d283921e7db77
FredericoVieira <fredops1@hotmail.com>
2021-11-20 18:58:10 -0300

    add animations

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import { Controller, Scene } from "react-scrollmagic";
+import { Tween, SplitLetters } from "react-gsap";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
@@ -25,119 +28,86 @@ export default function Home() {
             autoPlay
             muted
             loop
-            className={styles["video-content"]}
+            className={styles["video__content"]}
           >
             <source src="/video-intro.mp4" type="video/mp4" />
           </video>
         </section>
 
         <section className={styles.presentation}>
-          <img src="/presentation.png" alt="5531 apresentação" />
-          <div className={styles.content}>
-            <h1 className={styles.title}>Cachaça brasileira</h1>
-            <h2 className={styles["short-description"]}>
-              De um jeito que você nunca viu
-            </h2>
-            <p className={styles.description}>
+          <img
+            src="/presentation.png"
+            alt="5531 apresentação"
+            className={styles["presentation__image"]}
+          />
+          <div id="trigger" className={styles["presentation__content"]}>
+            <h1 className={styles["presentation__content__title"]}>
+              Cachaça brasileira
+            </h1>
+            <Controller>
+              <Scene classToggle="fadein" triggerElement="#trigger">
+                <h2
+                  className={styles["presentation__content__short-description"]}
+                >
+                  De um jeito que você nunca viu
+                </h2>
+              </Scene>
+            </Controller>
+            <p className={styles["presentation__content__description"]}>
               Bebiba mista de cachaça saborizada com os melhores ingredientes.
             </p>
-            <p className={styles.description}>
-              Ideal para dividir com pessoas ou suas personalidades. Sirva
-              gelada.
+            <p className={styles["presentation__content__description"]}>
+              Ideal para dividir com pessoas ou suas personalidades. <br />
+              Sirva gelada.
             </p>
           </div>
         </section>
 
         <section className={styles["shop-now"]}>
-          <h3 className={styles.title}>Para uma experiência única</h3>
+          <h3 className={styles["shop-now__title"]}>
+            Para uma experiência única
+          </h3>
           <a
             href="https://api.whatsapp.com/send?phone=5531984233332&text=Quero%20um%20experi%C3%AAncia%20%C3%BAnica!"
             target="blank"
+            className={styles["shop-now__link"]}
           >
-            <div className={styles.button}>Compre agora</div>
+            <div className={styles["shop-now__link__button"]}>Compre agora</div>
           </a>
         </section>
 
-        <section className={styles.product}>
-          <img src="/p1.png" alt="Cachaça honey" />
-          {/* <div className={styles.content}>
-            <h1 className={styles.title}>Cachaça brasileira</h1>
-            <h1 className={styles.description}>
-              De um jeito que você nunca viu
-            </h1>
-          </div> */}
-        </section>
+        <Controller globalSceneOptions={{ triggerHook: "onLeave" }}>
+          <Scene pin>
+            <section className={styles.product}>
+              <img
+                src="/cachaca-honey.png"
+                alt="Cachaça honey"
+                className={styles["product__image"]}
+              />
+            </section>
+          </Scene>
 
-        <section className={styles.product}>
-          <img src="/p2.png" alt="Cachaça" />
-          {/* <div className={styles.content}>
-            <h1 className={styles.title}>Cachaça brasileira</h1>
-            <h1 className={styles.description}>
-              De um jeito que você nunca viu
-            </h1>
-          </div> */}
-        </section>
+          <Scene pin>
+            <section className={styles.product}>
+              <img
+                src="/cachaca.png"
+                alt="Cachaça"
+                className={styles["product__image"]}
+              />
+            </section>
+          </Scene>
 
-        <section className={styles.product}>
-          <img src="/p3.png" alt="Cachaça pink-lemonade" />
-          {/* <div className={styles.content}>
-            <h1 className={styles.title}>Cachaça brasileira</h1>
-            <h1 className={styles.description}>
-              De um jeito que você nunca viu
-            </h1>
-          </div> */}
-        </section>
-
-        {/* <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
+          <Scene pin>
+            <section className={styles.product}>
+              <img
+                src="/cachaca-pink-lemonade.png"
+                alt="Cachaça pink-lemonade"
+                className={styles["product__image"]}
+              />
+            </section>
+          </Scene>
+        </Controller>
       </main>
-
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer> */}
     </div>
   );
 }
